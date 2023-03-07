837e5fe26adee40dbdf0c391e3ae477d0da56f2d
Djordje <83914973+djordje34@users.noreply.github.com>
2023-03-07 22:08:21 +0100

    Add files via upload

--- self_powers.py
+++ self_powers.py
@@ -16,7 +16,7 @@ Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
 def main():
     start_time = time.time()
     
-    print(f"{sum([x**x for x in range(1,1000)])}\nVreme: {time.time()-start_time} sekundi")
+    print(f"{(temp:=str(sum([x**x for x in range(1,1000)])))[len(temp)-10:]}\nVreme: {time.time()-start_time} sekundi")
     
 if __name__=='__main__':
     main()
